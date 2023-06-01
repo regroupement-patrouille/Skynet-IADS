3fcc9a28b944c920dd6034aecc07940c62492a2c
Florent Gasseau <florent.gasseau@metafactory.fr>
2023-06-01 17:01:51 +0200

    Branch initial

--- skynet-iads-source/skynet-iads-abstract-dcs-object-wrapper.lua
+++ skynet-iads-source/skynet-iads-abstract-dcs-object-wrapper.lua
@@ -18,7 +18,10 @@ end
 function SkynetIADSAbstractDCSObjectWrapper:setDCSRepresentation(representation)
 	self.dcsRepresentation = representation
 	if self.dcsRepresentation then
-		self.dcsName = self:getDCSRepresentation():getName()
+		self.dcsName = self.dcsRepresentation:getName() -- FG 11/05/2023 - baleBaron hotfix issue 81
+		if (self.dcsName == nil or string.len(self.dcsName) == 0) and self.dcsRepresentation.id_ then
+			self.dcsName = self.dcsRepresentation.id_
+		end
 	end
 end
 
